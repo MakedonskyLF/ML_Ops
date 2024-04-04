6309d5c9074e800900e074585b1285a6a953bbc1
MakedonskyLF <polyakov.main@gmail.com>
2024-04-04 20:45:03 +0300

    init

---
+++ tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit(1)
+
+
+def isComment(line: str) -> bool:
+    return line.lstrip().startswith(";")
+
+
+total = 0
+cur_length = 0
+
+lines = (
+    line.strip()
+    for line in open(sys.argv[1], "r")
+    if not isComment(line) and "E" in line
+)
+
+for line in lines:
+    tmp_length = float([leng[1:] for leng in line.split() if leng.startswith("E")][0])
+    if tmp_length > cur_length:
+        cur_length = tmp_length
+    else:
+        total += tmp_length
+        cur_length = 0
+print(total)
